b29fa9ac288d463c6337789c3f41f6a43968b1c6
prakhar-cse <prakhar.cse.gbu@gmail.com>
2025-06-14 22:14:50 +0530

    added external client:

--- com/java/CircuitBreaker/ExternalClient.java
+++ com/java/CircuitBreaker/ExternalClient.java
@@ -1,4 +1,19 @@
 package com.java.CircuitBreaker;
 
 public class ExternalClient {
+    private final CircuitBreaker circuitBreaker;
+
+    public ExternalClient() {
+        this.circuitBreaker = new CircuitBreaker(5, 6000);
+    }
+
+    public String CallExternalService() throws Exception{
+        return circuitBreaker.execute( ()->{
+            if(Math.random() >0.7){
+                return "success";
+            }else{
+                throw new RuntimeException("service unavaiable");
+            }
+        });
+    }
 }
