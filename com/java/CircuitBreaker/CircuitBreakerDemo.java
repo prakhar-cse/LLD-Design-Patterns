6a9aef9322e044dbb5802e77744f4a1f767cee7c
prakhar-cse <prakhar.cse.gbu@gmail.com>
2025-06-16 22:30:49 +0530

    added main class

--- com/java/CircuitBreaker/CircuitBreakerDemo.java
+++ com/java/CircuitBreaker/CircuitBreakerDemo.java
@@ -1,4 +1,25 @@
 package com.java.CircuitBreaker;
 
 public class CircuitBreakerDemo {
+
+    public static void main(String[] args){
+
+        ExternalClient client = new ExternalClient();
+
+        for(int i=0;i<25;i++){
+            try {
+                String result = client.CallExternalService();
+                System.out.println("call "+ (i+1)+": "+ result);
+            } catch (Exception e) {
+                System.out.println("call "+ (i+1)+": "+ e.getMessage());
+            }
+
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
